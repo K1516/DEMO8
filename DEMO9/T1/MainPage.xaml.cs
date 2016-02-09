eb298676d2de99b9868773790572c30e6173ec01
K1516 <k1516@student.jamk.fi>
2016-02-09 10:55:09 +0200

    UWP testing

---
+++ DEMO9/T1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace T1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+
+        private void Truck_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int trucks = int.Parse(textBlockTruck.Text);
+
+            trucks++;
+
+            textBlockTruck.Text = trucks.ToString();
+
+        }
+        
+        private void Car_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int cars = int.Parse(textBlockCar.Text);
+
+            cars++;
+
+            textBlockCar.Text = cars.ToString();
+            
+
+            
+                
+                
+        }
+    }
+}
